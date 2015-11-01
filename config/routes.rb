71f5ada1bd6e3dd8bea3a999f4eb0f8405884640
dannyalaska <dannyalaska@mac.com>
2015-11-01 12:04:53 -0500

    blogger commit one, created blog, added style

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :articles
+
+  # get "articles" => "articles#index"
+  # get "articles/new" => "articles#new"
+  # get "articles/:id" => "articles#show"
+  # post "articles" => "articles#create"
+
+end
